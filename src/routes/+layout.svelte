69a82ce1907809d24711ffd62a2eb3082bccee35
Olek Osikowicz <tupolewcx@gmail.com>
2024-03-06 18:08:05 +0000

    Mapbox hello world

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<header>It's header</header>
+<main>
+	<slot />
+</main>
+<footer>It's footer</footer>
--- src/routes/Map.svelte
+++ src/routes/Map.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
+	import mapboxgl from 'mapbox-gl'
+	import { onMount } from 'svelte'
+
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoicGFuendhcnp5d25pYWthIiwiYSI6ImNsdGcydzFtdTB4aDgyaXJ0cDBmZTl6aHMifQ.j3j7zHRSuFDj2maiwwvgVA'
+
+	onMount(async () => {
+		const map = new mapboxgl.Map({
+			container: 'map', // container ID
+			style: 'mapbox://styles/mapbox/streets-v12', // style URL
+			center: [-74.5, 40], // starting position [lng, lat]
+			zoom: 9 // starting zoom
+		})
+	})
 </script>
 
-Dupa
+<div id="map"></div>
 
 <style>
+	#map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+	}
 </style>
