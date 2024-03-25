1f0657243059b1160061b3e4e7a4637a97582ae8
Olek Osikowicz <tupolewcx@gmail.com>
2024-03-25 18:44:09 +0000

    Remove generate part

--- src/routes/generate/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	// import './original/renderer'
-</script>
-
-<div class="container" id="map"></div>
-<h1>Sandbox</h1>
--- src/routes/generate/main.js
+++
@@ -1,8 +0,0 @@
-export const prerender = false;
-
-// var mapboxgl = require('mapbox-gl/dist/mapbox-gl-dev.js')
-// import {mapboxgl} from 'mapbox-gl-old/dist/mapbox-gl-dev.js'
-import {mapboxgl} from "mapbox-gl-old";
-// import './renderer'
-console.log(mapboxgl)
-console.log("Siema")
