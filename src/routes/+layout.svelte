21b39b69e2a23d85b35587321778c713ce25bb0d
Olek Osikowicz <tupolewcx@gmail.com>
2024-04-18 20:08:44 +0100

    Better layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
-<header class="container">
-	<nav>
-		<a href="/">Logo</a>
-	</nav>
-</header>
-<main class="container">
-	<slot />
-</main>
-<footer class="container">It's footer</footer>
+<div id="page-container">
+	<header class="container">
+		<nav>
+			<h1>Pretty map generator</h1>
+			<a href="/">Logo</a>
+		</nav>
+	</header>
+	<main class="container">
+		<slot />
+	</main>
+	<footer class="container">
+		<p>It's footer</p>
+	</footer>
+</div>
+
+<style>
+	#page-container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	header {
+		margin-bottom: auto;
+	}
+	footer {
+		margin-top: auto;
+	}
+</style>
