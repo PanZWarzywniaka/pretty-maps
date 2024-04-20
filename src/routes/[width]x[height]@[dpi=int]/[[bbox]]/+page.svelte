1ff2940a3a948bb0060298fbc135e16ccaf01d4f
Olek Osikowicz <tupolewcx@gmail.com>
2024-04-20 17:46:15 +0100

    Add better loading screen and display generated map in modal

--- src/routes/[width]x[height]@[dpi=int]/[[bbox]]/+page.svelte
+++ src/routes/[width]x[height]@[dpi=int]/[[bbox]]/+page.svelte
@@ -2,11 +2,24 @@
 	export let data
 </script>
 
-{#await data.url}
-	<p>Rendering your map...</p>
-	<p>It may take 30seconds</p>
-{:then url}
-	<img src={url} alt="Your map render" />
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<article>
+	{#await data.url}
+		<span aria-busy="true">Generating your map...</span>
+	{:then url}
+		<dialog open>
+			<article>
+				<header>
+					<p>
+						<strong>🗓️ Thank You for using Pretty Maps!</strong>
+					</p>
+				</header>
+				<img src={url} alt="Your map render" />
+				<footer>
+					<a href="/">Generate more maps!</a>
+				</footer>
+			</article>
+		</dialog>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+</article>
